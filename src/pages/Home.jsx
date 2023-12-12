ef4dbbc9779be29aff1a824b3d7b7121d22c5e62
loozeevert <151251297+loozeevert@users.noreply.github.com>
2023-12-12 14:45:43 +0300

    vvit lab 4

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Button, Center, Text, Stack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { saveAs } from 'file-saver';
+
+export const Home = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const exportTasks = () => {
+        const tasksJSON = JSON.stringify(tasks);
+        const blob = new Blob([tasksJSON], { type: 'application/json' });
+        saveAs(blob, 'tasks.json');
+    };
+
+    const importTasks = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const importedTasks = JSON.parse(e.target.result);
+                setTasks(importedTasks);
+            };
+            reader.readAsText(file);
+        }
+    };
+
+    return (
+        <Center h="100vh">
+            <Stack>
+                <Text textAlign="center">Страница Home</Text>
+                <Button as={Link} to="/asdfg" colorScheme="blue">
+                    Перейти на страницу 404
+                </Button>
+                <Button onClick={exportTasks} colorScheme="green">
+                    Экспорт задач
+                </Button>
+                <input type="file" accept=".json" onChange={importTasks} />
+                <Text>Выберите файл для импорта задач</Text>
+            </Stack>
+        </Center>
+    );
+};
